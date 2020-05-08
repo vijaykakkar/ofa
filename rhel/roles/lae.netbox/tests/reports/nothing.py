75822f19ef04005f2db83a2c51af204afd378778
Vijay Kakkar <vijaykakkars@gmail.com>
2020-05-08 16:18:37 +0000

    Netbox Roles Removed

--- rhel/roles/lae.netbox/tests/reports/nothing.py
+++
@@ -1,8 +0,0 @@
-from extras.reports import Report
-
-
-class NothingReport(Report):
-    description = "A report."
-
-    def test_nothing(self):
-        self.log_success("Success! Hm? What is, you ask? That's a secret.")
--- rhel/roles/lae.netbox/tests/scripts/nothing.py
+++
@@ -1,6 +0,0 @@
-from extras.scripts import Script
-
-
-class Nothing(Script):
-    def run(self, data, commit=False):
-        return ""
